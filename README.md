b0ddbb236b5c8508e6f8dd0252e489a5b63cc679
Jeff Marion <jeff@marion.mx>
2019-09-05 23:23:01 -0700

    v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # json-seq
 Tools for working with json-seq (RFC 7464)
+
+## Stream Options
+
+Both of the Stream types are instance of [stream.Transform](https://nodejs.org/api/stream.html#stream_class_stream_transform) and pass through all stream.Transform options.
+
+They additionally support charCodeStart and charCodeEnd parameters which can be used to override the default RFC 7464 record start and end characters.
+
+## ParseStream
+
+```javascript
+import jsonSeq from 'json-seq';
+
+const parseStream = new jsonSeq.ParseStream();
+parseStream.on('data', record => {
+  console.log('Value of "foo"', record.foo);
+});
+parseStream.on('invalid', string => {
+  console.log('Invalid record encountered. Record string: ', string);
+});
+parseStream.on('truncated', string => {
+  console.log('Truncated record encountered. Record string: ', string);
+});
+
+parseStream.write('{"foo": "bar"}\n');
+
+```
+
+## StringifyStream
+
+```javascript
+import jsonSeq from 'json-seq';
+
+const stringifyStream = new jsonSeq.StringifyStream();
+stringifyStream.pipe(process.stdout);
+
+stringifyStream.write({foo: "bar"});
+```
---
+++ src/index.js
@@ -0,0 +1,150 @@
+const {Buffer} = require('buffer');
+const {Transform} = require('stream');
+
+const CHAR_CODE_RECORD_SEPARATOR = 30;
+const CHAR_CODE_NEWLINE = 10;
+
+/**
+ * Invalid event.
+ *
+ * Emitted when the content between the start and end characters isn't valid JSON.
+ *
+ * @event invalid
+ * @type {string}
+ */
+
+/**
+ * Truncated event.
+ *
+ * Emitted when a record isn't terminated by an end character
+ * before the next start character is encountered.
+ *
+ * @event truncated
+ * @type {string}
+ */
+
+/**
+ * Parse Stream
+ *
+ * @fires invalid
+ * @fires truncated
+ */
+class ParseStream extends Transform {
+  /**
+   * @param options
+   * @param options.charCodeStart Character code that marks the start of a record (defaults to RS)
+   * @param options.charCodeEnd   Character code that marks the end of a record (default to newline)
+   */
+  constructor(options) {
+    const mergedOptions = {
+      charCodeStart: CHAR_CODE_RECORD_SEPARATOR,
+      charCodeEnd: CHAR_CODE_NEWLINE,
+      readableObjectMode: true,
+      ...options,
+    };
+    super(mergedOptions);
+
+    this.charCodeStart = mergedOptions.charCodeStart;
+    this.charCodeEnd = mergedOptions.charCodeEnd;
+
+    this.buffer = [];
+  }
+
+  _bufferContent(content, startIndex = 0, endIndex = undefined) {
+    this.buffer.push(content.slice(startIndex, endIndex));
+  }
+
+  _transform(chunk, encoding, callback) {
+    let chunkIndexStart = undefined;
+
+    for (let chunkIndex = 0; chunkIndex < chunk.length; ++chunkIndex) {
+      const char = chunk[chunkIndex];
+
+      if (char === this.charCodeStart) {
+        if (this.buffer.length) {
+          this.emit('truncated', Buffer.concat(this.buffer).toString('utf8'));
+
+          this.buffer = [];
+        }
+
+        chunkIndexStart = chunkIndex + 1;
+      } else if (char === this.charCodeEnd) {
+        if (chunkIndexStart === undefined && !this.buffer.length) {
+          continue;
+        }
+
+        const newContentStartIndex =
+          chunkIndexStart !== undefined ? chunkIndexStart : 0;
+        if (chunkIndex > newContentStartIndex) {
+          this._bufferContent(chunk, newContentStartIndex, chunkIndex);
+        }
+
+        const string = Buffer.concat(this.buffer).toString('utf8');
+        try {
+          const data = JSON.parse(string);
+
+          this.push(data);
+        } catch (err) {
+          this.emit('invalid', string);
+        }
+
+        this.buffer = [];
+        chunkIndexStart = undefined;
+      }
+    }
+
+    if (chunkIndexStart !== undefined) {
+      this._bufferContent(chunk, chunkIndexStart);
+    } else if (this.buffer.length) {
+      this._bufferContent(chunk);
+    }
+
+    callback();
+  }
+}
+
+/**
+ * Stringify Stream
+ */
+class StringifyStream extends Transform {
+  /**
+   * @param options
+   * @param options.charCodeStart Character code that marks the start of a record (defaults to RS)
+   * @param options.charCodeEnd   Character code that marks the end of a record (default to newline)
+   */
+  constructor(options) {
+    const mergedOptions = {
+      charCodeStart: CHAR_CODE_RECORD_SEPARATOR,
+      charCodeEnd: CHAR_CODE_NEWLINE,
+      writeableObjectMode: true,
+      ...options,
+    };
+    super(mergedOptions);
+
+    this.charCodeStart = mergedOptions.charCodeStart;
+    this.charCodeEnd = mergedOptions.charCodeEnd;
+  }
+
+  _transform(chunk, encoding, callback) {
+    try {
+      const string = JSON.stringify(chunk);
+
+      if (this.charCodeStart) {
+        this.push(this.charCodeStart);
+      }
+      this.push(string);
+      if (this.charCodeEnd) {
+        this.push(this.charCodeEnd);
+      }
+
+      callback();
+    } catch (err) {
+      callback(err);
+    }
+  }
+}
+
+module.exports = {
+  ParseStream,
+  StringifyStream,
+};
